b9df1f7aabbf5c54b8f86e3db292b025c6f23608
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2019-10-15 22:17:11 +0100

    - Refactored

--- src/main/scala/RomanNumerals/RomanNumerals.scala
+++ src/main/scala/RomanNumerals/RomanNumerals.scala
@@ -1,42 +1,41 @@
 package RomanNumerals
 
-case class NumeralMap(value: Int, numerals: String)
-
-object NumeralMap {
-  implicit def apply(map: (Int, String)): NumeralMap = NumeralMap(map._1, map._2)
+case class NumeralMap(value: Int, numerals: String) {
+  def isLessOrEqual(x: Int): Boolean = value <= x
 }
 
-object RomanNumerals {
+trait NumeralMapConvert {
+  implicit def apply(map: (Int, String)): NumeralMap = map match {
+    case (value, numeral) => NumeralMap(value, numeral)
+  }
+}
 
+object RomanNumerals extends NumeralMapConvert {
   def toRomanNumerals(number: Int): String = {
+    val numeralMatch = mappings.find(_.isLessOrEqual(number))
 
-    val mappings = Seq[NumeralMap](
-      1 -> "I",
-      2 -> "II",
-      3 -> "III",
-      4 -> "IV",
-      5 -> "V",
-      6 -> "VI",
-      7 -> "VII",
-      8 -> "VIII",
-      9 -> "IX",
-      10 -> "X",
-      50 -> "L",
-      90 -> "XC",
-      100 -> "C",
-      500 -> "D",
-      900 -> "CM",
-      1000 -> "M"
-    ).reverse
-
-    val mapping = mappings
-      .find {
-        case NumeralMap(value, _) => number - value >= 0
-      }
-
-    mapping match {
-      case Some(mapping) => mapping.numerals + toRomanNumerals(number - mapping.value)
+    numeralMatch match {
+      case Some(numeralMatch) => numeralMatch.numerals + toRomanNumerals(number - numeralMatch.value)
       case None => ""
     }
   }
+
+  private val mappings = Seq[NumeralMap](
+    1 -> "I",
+    2 -> "II",
+    3 -> "III",
+    4 -> "IV",
+    5 -> "V",
+    6 -> "VI",
+    7 -> "VII",
+    8 -> "VIII",
+    9 -> "IX",
+    10 -> "X",
+    50 -> "L",
+    90 -> "XC",
+    100 -> "C",
+    500 -> "D",
+    900 -> "CM",
+    1000 -> "M"
+  ).reverse
 }
